c5a2a68a27cb179caaa83b08b28df7d5b451dac6
HugoBzn <hugobazan1499@gmail.com>
2023-07-02 15:10:11 -0600

    Get data from the API

--- src/views/ClientesView.vue
+++ src/views/ClientesView.vue
@@ -1,6 +1,18 @@
 <script setup>
+import { onMounted, ref } from "vue";
+
+import axios from "axios";
+
 import RouterLink from "../components/UI/RouterLink.vue";
 import Title from "../components/UI/Title.vue";
+
+const clientes = ref([]);
+
+onMounted(() => {
+  axios("http://localhost:4000/clientes")
+    .then(({ data }) => (clientes.value = data))
+    .catch((error) => console.log("Hubo un error"));
+});
 
 defineProps({
   titleProp: {
