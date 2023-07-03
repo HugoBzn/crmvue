4c54eab321c99dc382129546f0f4b0c78dbea829
HugoBzn <hugobazan1499@gmail.com>
2023-07-02 18:52:01 -0600

    New favicon and style to crossbar

--- src/App.vue
+++ src/App.vue
@@ -13,3 +13,25 @@ import Header from "./components/UI/Header.vue";
     <RouterView />
   </main>
 </template>
+
+<style lang="css">
+/* width */
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #fff;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #1e293b;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #0b1016;
+}
+</style>
